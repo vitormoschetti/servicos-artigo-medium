0c953a8473d39bbdc9fa373fb273714b1b7f3441
Vitor Moschetti <vitormoschetti@terra.com.br>
2020-09-03 15:09:44 -0300

    crud

--- livro/livro/src/main/java/br/com/artigo/livro/controller/LivroController.java
+++ livro/livro/src/main/java/br/com/artigo/livro/controller/LivroController.java
@@ -3,7 +3,11 @@ package br.com.artigo.livro.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,43 +15,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import br.com.artigo.livro.entity.Categoria;
 import br.com.artigo.livro.entity.Livro;
 import br.com.artigo.livro.entity.Modelo;
+import br.com.artigo.livro.repository.LivroRepository;
 
 @Controller
 public class LivroController {
+	
+	@Autowired
+	private LivroRepository livroRepository;
 
 	@ResponseBody
 	@RequestMapping("/livros")
 	public List<Livro> listar() {
 
-		Livro livro = new Livro(123L, "Controller - Spring", 1, "Vitor", Categoria.INFORMATICA,
-				Arrays.asList(Modelo.EBOOK, Modelo.FISICO));
+		Livro livro = new Livro(123L, "Controller - Spring", 1, "Vitor", Categoria.INFORMATICA, Modelo.EBOOK);
 
 		return Arrays.asList(livro, livro, livro);
 
 	}
 
 	@ResponseBody
-	@RequestMapping(name = "/livros", method = RequestMethod.POST)
-	public Livro salvar(Livro livro) {
-
-		return null;
-
-	}
-
-	@ResponseBody
-	@RequestMapping(name = "/livros", method = RequestMethod.PUT)
-	public Livro atualizar(Livro livro) {
-
-		return null;
-
-	}
-
-	@ResponseBody
-	@RequestMapping(name = "/livros", method = RequestMethod.DELETE)
-	public Livro deletar(Long isbn) {
-
-		return null;
-
+	@Transactional
+	@RequestMapping(path = "/livros", method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		
+		livroRepository.save(livro);
 	}
 
 }
--- livro/livro/src/main/java/br/com/artigo/livro/entity/Livro.java
+++ livro/livro/src/main/java/br/com/artigo/livro/entity/Livro.java
@@ -1,22 +1,30 @@
 package br.com.artigo.livro.entity;
 
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Entity
 @Getter
 @ToString
+@NoArgsConstructor
 @AllArgsConstructor
 public class Livro {
-	
+
+	@Id
 	private Long isbn;
 	private String titulo;
 	private Integer edicao;
 	private String autor;
+	@Enumerated(EnumType.STRING)
 	private Categoria categoria;
-	private List<Modelo> modelo;
-	
+	@Enumerated(EnumType.STRING)
+	private Modelo modelo;
 
 }
