256ac319ddf98d97d4d75d38968bbee44f4390d4
vitormoschetti <vitormoschetti@terra.com.br>
2020-09-15 07:57:43 -0300

    CRUD completo - artigo primeiro crud
    CRUD sem boas práticas

--- livro/livro/src/main/java/br/com/artigo/livro/controller/LivroController.java
+++ livro/livro/src/main/java/br/com/artigo/livro/controller/LivroController.java
@@ -1,50 +1,52 @@
 package br.com.artigo.livro.controller;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
-import javax.transaction.Transactional;
-
+import br.com.artigo.livro.entity.Livro;
+import br.com.artigo.livro.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-import br.com.artigo.livro.entity.Categoria;
-import br.com.artigo.livro.entity.Livro;
-import br.com.artigo.livro.entity.Modelo;
-import br.com.artigo.livro.repository.LivroRepository;
 import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Controller
 public class LivroController {
-	
-	@Autowired
-	private LivroRepository livroRepository;
 
-	@ResponseBody
-	@RequestMapping("/livros")
-	public List<Livro> listar() {
-		List<Livro> livros = livroRepository.findAll();
-		return livros;
-	}
+    @Autowired
+    private LivroRepository livroRepository;
 
-	@ResponseBody
-	@Transactional
-	@RequestMapping(path = "/livros", method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
+    @ResponseBody
+    @RequestMapping("/livros")
+    public List<Livro> listar() {
+        List<Livro> livros = livroRepository.findAll();
+        return livros;
+    }
 
-		livroRepository.save(livro);
-	}
+    @ResponseBody
+    @Transactional
+    @RequestMapping(path = "/livros", method = RequestMethod.POST)
+    public void salvar(@RequestBody Livro livro) {
 
-	@ResponseBody
-	@Transactional
-	@RequestMapping(path = "/livros", method = RequestMethod.PUT)
-	public void atualizar(@RequestBody Livro livro) {
+        livroRepository.save(livro);
+    }
 
-		livroRepository.save(livro);
+    @ResponseBody
+    @Transactional
+    @RequestMapping(path = "/livros", method = RequestMethod.PUT)
+    public void atualizar(@RequestBody Livro livro) {
 
-	}
+        livroRepository.save(livro);
 
+    }
+
+    @ResponseBody
+    @Transactional
+    @RequestMapping(path = "/livros/{isbn}", method = RequestMethod.DELETE)
+    public void deletar(@PathVariable Long isbn) {
+
+        livroRepository.deleteById(isbn);
+
+    }
 
 
 }
